7b8fad1256cd495bd7c86e852e4bbed55ffc1555
Luca D'Angelo <dangelo.luca@iisgalvanimi.edu.it>
2025-03-26 10:26:21 +0000

    mappa vidibile ma non viene caricata del tutto

--- mappa/src/app/app.component.ts
+++ mappa/src/app/app.component.ts
@@ -1,22 +1,10 @@
 import { Component } from '@angular/core';
 import {
   Map,
-  Control,
-  DomUtil,
-  ZoomAnimEvent,
-  Layer,
+
   MapOptions,
   tileLayer,
   latLng,
-  LeafletEvent,
-  icon,
-  marker,
-  LatLng,
-  Icon,
-  IconOptions,
-  MarkerOptions,
-  LayerOptions,
-  Marker,
 } from 'leaflet';
 
 @Component({
@@ -29,10 +17,7 @@ export class AppComponent {
 
   mouseX: number = 0;
   mouseY: number = 0;
-  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger | undefined;
-  @Output() map$: EventEmitter<Map> = new EventEmitter();
-  @Output() zoom$: EventEmitter<number> = new EventEmitter();
-  @Input() options: MapOptions = {
+  options: MapOptions = {
     layers: [
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         opacity: 0.7,
@@ -43,32 +28,8 @@ export class AppComponent {
       }),
     ],
     zoom: 15,
-    center: latLng(-22.37855150766163, -41.778323650360115),
+    center: latLng(45.464211, 9.191383),
   };
-  public map: Map | undefined;
-  public zoom: number | undefined;
-  markerIconBlue = icon({
-    iconSize: [25, 41],
-    iconAnchor: [10, 41],
-    popupAnchor: [2, -40],
-    // specify the path here
-    iconUrl:
-      'https://raw.githubusercontent.com/raulmu/assets/main/marker-icon-blue.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png',
-  }) as Icon<IconOptions>;
-  markerIconRed = icon({
-    iconSize: [25, 41],
-    iconAnchor: [10, 41],
-    popupAnchor: [2, -40],
-    // specify the path here
-    iconUrl:
-      'https://raw.githubusercontent.com/raulmu/assets/main/marker-icon-red.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png',
-  }) as Icon<IconOptions>;
-  markers: Marker[] = [];
-  menuMarker: Marker | undefined;
-  constructor() {}
-
-
+ 
 
 }
--- mappa/src/app/app.module.ts
+++ mappa/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
 import { AppComponent } from './app.component';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+
+    LeafletModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
